6309d91cc9e4056c0773c264b4629a2842a78bfb
cgabriel5 <carlosgabriel71@yahoo.com>
2017-09-27 20:35:33 -0700

    fix: Update libs.js
    Update the libs.js to include the third-party library initiation
    code.

--- gulp/setup/js/options/library/bundles/libs.js
+++ gulp/setup/js/options/library/bundles/libs.js
@@ -688,3 +688,12 @@
         window.FastClick = FastClick;
     }
 })();
+(function() {
+    // initialize 3rd-party libraries
+    // init FastClickJS
+    if ("addEventListener" in document) {
+        document.addEventListener("DOMContentLoaded", function() {
+            FastClick.attach(document.body);
+        }, false);
+    }
+})();
