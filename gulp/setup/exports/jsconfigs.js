40b8b3d7cc3ce230c6a5e55f81e49b6eddb4d70a
cgabriel5 <carlosgabriel71@yahoo.com>
2017-09-30 19:12:17 -0700

    fix: Update jsconfigs.js
    Update the needed paths to the correct ones.

--- gulp/setup/exports/jsconfigs.js
+++ gulp/setup/exports/jsconfigs.js
@@ -2,24 +2,24 @@ exports.jsconfigs = {
     "library": {
         "source": {
             "files": [
-                    "lib.iife.top.js",
-                    "lib.library.top.js",
-                    "modules/fn.helpers.js",
-                    "modules/fn.source.js",
-                    "modules/fn.core.js",
-                    "modules/constructor.js",
-                    "lib.library.end.js",
-                    "modules/globals.js",
-                    "modules/bottom.js",
-                    "lib.iife.end.js",
-                    "test.js"
+                "lib.iife.top.js",
+                "lib.library.top.js",
+                "modules/fn.helpers.js",
+                "modules/fn.source.js",
+                "modules/fn.core.js",
+                "modules/constructor.js",
+                "lib.library.end.js",
+                "modules/globals.js",
+                "modules/bottom.js",
+                "lib.iife.end.js",
+                "test.js"
                 ],
             "name": "app.js"
         },
         "thirdparty": {
             "files": [
-                    "fastclick.js",
-                    "libs.init.js"
+                "js/libs/fastclick/lib/fastclick.js",
+                "js/libs/libs.init.js"
                 ],
             "name": "libs.js",
             "minified_name": "lib.min.js"
@@ -28,30 +28,30 @@ exports.jsconfigs = {
     "webapp": {
         "source": {
             "files": [
-                    "app.iife.top.js",
-                    "app.init.js",
-                    "modules/libs.js",
-                    "modules/globals.js",
-                    "modules/utils.js",
-                    "modules/$$.js",
-                    "modules/core.js",
-                    "modules/events.js",
-                    "modules/main.js",
-                    "app.iife.end.js"
+                "app.iife.top.js",
+                "app.init.js",
+                "modules/libs.js",
+                "modules/globals.js",
+                "modules/utils.js",
+                "modules/$$.js",
+                "modules/core.js",
+                "modules/events.js",
+                "modules/main.js",
+                "app.iife.end.js"
                 ],
             "name": "app.js"
         },
         "thirdparty": {
             "files": [
-                    "fastclick.js"
+				"js/libs/fastclick/lib/fastclick.js"
                 ],
             "name": "libs.js"
         }
     },
     "lib": {
         "tasks": [
-                "task-lib-clean",
-                "task-lib-js"
-            ]
+            "task-lib-clean",
+            "task-lib-js"
+		]
     }
 };
