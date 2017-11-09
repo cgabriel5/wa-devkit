1ecaecc412ea3ff600947c6a5ecdfc73490f6117
cgabriel5 <carlosgabriel71@yahoo.com>
2017-11-08 20:11:49 -0800

    refactor: Update the ext() helper function
    - The function now takes an array of file extension types to check
    against.
    - Make sure to remove the . before the extension.

--- gulp/source/functions.js
+++ gulp/source/functions.js
@@ -131,34 +131,37 @@ function globall(string) {
 /**
  * @description [Returns the provided file's extension or checks it against the provided extension type.]
  * @param  {Object} file [The Gulp file object.]
- * @param  {String} type [The optional extension type to check against.]
+ * @param  {Array} types [The optional extension type(s) to check against.]
  * @return {String|Boolean}      [The file's extension or boolean indicating compare result.]
  */
-function ext(file, type) {
+function ext(file, types) {
 	// when no file exists return an empty string
 	if (!file) return "";
 
 	// get the file extname
-	var extname = path.extname(file.path).toLowerCase();
+	var extname = path
+		.extname(file.path)
+		.toLowerCase()
+		.replace(/^\./, "");
 
 	// simply return the extname when no type is
 	// provided to check against.
-	if (!type) return extname;
+	if (!types) return extname;
 
 	// else when a type is provided check against it
-	return extname.slice(1) === type.toLowerCase();
+	return Boolean(-~types.indexOf(extname));
 }
 
 // check for the usual file types
 ext.ishtml = function(file) {
-	return ext(file, "html");
+	return ext(file, ["html"]);
 };
 ext.iscss = function(file) {
-	return ext(file, "css");
+	return ext(file, ["css"]);
 };
 ext.isjs = function(file) {
-	return ext(file, "js");
+	return ext(file, ["js"]);
 };
 ext.isjson = function(file) {
-	return ext(file, "json");
+	return ext(file, ["json"]);
 };
