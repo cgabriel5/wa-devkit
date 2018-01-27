7a9bddc0fda00517e602c4f2daff06297439872f
cgabriel5 <carlosgabriel71@yahoo.com>
2018-01-27 12:52:29 -0800

    feat: Update the open (file editing/opening file manager)
    - Update the open task to be able to handle opening the file manager
    when a directory is provided and opening the user's text editor when
    needing to edit a file on the fly.
    - Add a get_editor() function to functions.js that gets the user's
    default text editor.

--- gulp/main/source/functions.js
+++ gulp/main/source/functions.js
@@ -43,3 +43,81 @@ function gulp_check_warn() {
 		)
 	);
 }
+
+function get_editor(options) {
+	// Default options
+	options = options || {};
+
+	// Use the provided editor or get the environment variables
+	var editor = options.editor || process.env.EDITOR || process.env.VISUAL;
+
+	// Default to the tried and true editors when nothing is found.
+	if (!editor) editor = /^win/.test(process.platform) ? "notepad" : "vim";
+
+	// Lowercase everything
+	editor = editor.toLowerCase();
+
+	// If nothing is found check the check the Git config??
+
+	// If an editor is found in an environment variable it will
+	// simply be a command followed by a flag(s). For example,
+	// this it could be something like this: "subl -w -n". "subl"
+	// being the editor command and "-w -n" the flags to use.
+
+	// Get any flags.
+	var flags = [];
+	if (options.flags) {
+		// Add the provided flags to the flags array.
+		flags = flags.concat(options.flags);
+	}
+
+	// Now get any flags found in the editor string
+	var parts = editor.split(/\s+/);
+	// Flags exist.
+	if (parts.length > 1) {
+		// Reset the editor variable and remove the editor from the
+		// parts array.
+		editor = parts.shift();
+		// Add all the flags to the flags array.
+		flags = flags.concat(parts);
+	} // Else there only exists an editor in the string
+
+	// Add other needed flags to make this work...
+	// Code lifted and modified from here:
+	// [https://github.com/sindresorhus/open-editor]
+
+	// Get the file parts
+	var file = options.file;
+	var name = file.name;
+	var line = file.line || 1;
+	var column = file.column || 1;
+
+	// Visual Studio Code needs a flag to open file at line number/column.
+	if (-~["code"].indexOf(editor)) {
+		flags.push("--goto");
+	}
+
+	// Add needed flags depending on the editor being used.
+	if (-~["atom", "code"].indexOf(editor) || /^subl/.test(editor)) {
+		// Open in a new window and wait for the file to close.
+		flags.push("--new-window", "--wait", `${name}:${line}:${column}`);
+	} else if (editor === "gedit") {
+		// gedit --new-window --wait ./configs/bundles.json +135:1
+		flags.push("--new-window", "--wait", name, `+${line}:${column}`);
+	} else if (-~["webstorm", "intellij"].indexOf(editor)) {
+		flags.push(`${name}:${line}`);
+	} else if (editor === "textmate") {
+		flags.push("--line", `${line}:${column}`, name);
+	} else if (-~["vim", "neovim"].indexOf(editor)) {
+		flags.push(`+call cursor(${line}, ${column})`, name);
+	} else {
+		// Default to pushing the file only
+		flags.push(name);
+	}
+
+	// Return the editor command with the flags to apply.
+	return {
+		command: editor,
+		flags: flags
+	};
+}
--- gulp/main/source/helpers/open.js
+++ gulp/main/source/helpers/open.js
@@ -1,5 +1,4 @@
 /**
- * task: open
  * Opens provided file in browser.
  *
  * Notes
@@ -15,16 +14,57 @@
  *
  * -f, --file
  *     <file> The path of the file to open.
+ *
  * -p, --port
  *     [number] The port to open in. (Defaults to browser-sync port if
  *     available or no port at all.)
+ *
+ * -d, --directory
+ *     [string] The directory path to open in a file manager.
+ *
+ * -e, --editor
+ *     [string] The file path to open in the user's text editor to edit.
+ *
+ * --wait
+ *     [boolean] To be Used with the -e/--editor flag. If provided the
+ *     editor will wait to close and will only close manually (i.e.
+ *     close the editor or exit the terminal task).
+ *
+ * --line
+ *     [number] To be used with -e/--editor flag. Open the file at the
+ *     provided line.
+ *
+ * --column
+ *     [number] To be used with -e/--editor flag. Open the file at the
+ *     provided column.
+ *
+ * --use
+ *     [string] To be used with -e/--editor flag. Manually set the editor
+ *     to use. Will default to the user's default editor via ($EDITOR/$VISUAL)
+ *     environment variables.
  *
  * Usage
  *
  * $ gulp open --file index.html --port 3000
  *     Open index.html in port 3000.
+ *
  * $ gulp open --file index.html
  *     Open index.html in browser-sync port is available or no port.
+ *
+ * $ gulp open --editor ./index.html --wait --line 12 --column 20 --use atom
+ *     Open "./index.html" using the text editor Atom if available. Set
+ *     the line to 12 and column 20. Use the --wait flag to close the process
+ *     after the editor is close or the process is killed via the terminal.
+ *
+ * $ gulp open --directory .
+ *     Open the root directory in a file manager.
+ *
+ * $ gulp open --directory ./docs
+ *     Open the docs directory in a file manager.
+ *
+ * $ gulp open --directory docs/subextensions.md
+ *     When a file is provided along with the directory, only the directory
+ *     section of the path will be used to try and open in a file manager.
  */
 gulp.task("open", function(done) {
 	// cache task
@@ -32,29 +72,151 @@ gulp.task("open", function(done) {
 
 	// run yargs
 	var _args = yargs
-		.option("file", {
-			alias: "f",
-			demandOption: true,
+		.option("directory", {
+			alias: "d",
 			type: "string"
 		})
-		.option("port", {
-			alias: "p",
-			type: "number"
+		.option("editor", {
+			alias: "e",
+			type: "string"
 		}).argv;
 
 	// get the command line arguments from yargs
-	var file = _args.f || _args.file;
-	// check for explicitly provided port...if none is provided
-	// check the internally fetched free ports and get the local port
-	var port =
-		_args.p ||
-		_args.port ||
-		(
-			$internal.get("ports") || {
-				local: null
-			}
-		).local;
+	// Open the root when nothing provided.
+	var directory = _args.d || _args.directory;
+	var editor = _args.e || _args.editor;
 
-	// run the open function
-	return open_file_in_browser(file, port, done, task);
+	// If the directory flag is provided open the directory in a file
+	// manager.
+	if (directory) {
+		// Parse the directory.
+		var parts = path.parse(directory);
+		if (!parts.ext) {
+			// No file was passed in.
+			// Reset the directory
+			directory = parts.dir + "/" + parts.base + "/";
+		} else {
+			// If a file is passed only get the dir part.
+			directory = parts.dir + "/";
+		}
+
+		// Make the path absolute and relative to the main project root.
+		directory = path.join("./", directory);
+
+		// Check that the directory exists
+		if (!de.sync(directory)) {
+			print.gulp(
+				chalk.yellow("The directory:"),
+				chalk.magenta(directory),
+				chalk.yellow("does not exist.")
+			);
+			return done();
+		}
+
+		// Else the directory exists so open the file manager.
+		require("opener")(directory, function() {
+			done();
+		});
+	} else if (editor) {
+		var spawn = require("child_process").spawn;
+
+		// Check that the file exists.
+		if (!fe.sync(editor)) {
+			print.gulp(
+				chalk.yellow("The file:"),
+				chalk.magenta(directory),
+				chalk.yellow("does not exist.")
+			);
+			return done();
+		}
+
+		// run yargs
+		var _args = yargs
+			.option("wait", {
+				type: "boolean"
+			})
+			.option("line", {
+				type: "number"
+			})
+			.option("column", {
+				type: "number"
+			})
+			.option("use", {
+				type: "string"
+			}).argv;
+
+		// Get the command line arguments from yargs
+		var wait = _args.wait;
+		var line = _args.line;
+		var column = _args.column;
+		var use_editor = _args.use;
+
+		// Get the user's editor and any flags needed to open the
+		// file via the terminal.
+		var editor = get_editor({
+			file: {
+				name: editor,
+				line: line,
+				column: 0
+			},
+			editor: use_editor
+		});
+
+		// Create the child process to open the editor
+		var child_process = spawn(editor.command, editor.flags, {
+			stdio: "inherit",
+			detached: true
+		});
+
+		// If an error occurs throw it
+		child_process.on("error", function(err) {
+			if (err) {
+				throw err;
+			}
+		});
+
+		// If the wait flag is provided make the process hold until the
+		// user closes the file or the terminal process is ended manually.
+		if (wait) {
+			// Once the file is closed continue with the task...
+			child_process.on("exit", function(code, sig) {
+				done();
+			});
+		} else {
+			// Close the process immediately.
+			child_process.unref();
+			return done();
+		}
+	} else {
+		// Else open the file in a browser. What this task was originally
+		// set out to do.
+
+		// run yargs
+		var _args = yargs
+			.option("file", {
+				alias: "f",
+				demandOption: true,
+				type: "string"
+			})
+			.option("port", {
+				alias: "p",
+				type: "number"
+			}).argv;
+
+		// get the command line arguments from yargs
+		var file = _args.f || _args.file;
+		// check for explicitly provided port...if none is provided
+		// check the internally fetched free ports and get the local port
+		var port =
+			_args.p ||
+			_args.port ||
+			(
+				$internal.get("ports") || {
+					local: null
+				}
+			).local;
+
+		// run the open function
+		return open_file_in_browser(file, port, done, task);
+	}
 });
