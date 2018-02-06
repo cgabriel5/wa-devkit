0f88d4b2f73cf725275de2de9abd15d0b8929630
cgabriel5 <carlosgabriel71@yahoo.com>
2018-02-06 12:20:48 -0800

    refactor: Re-implement the stats
    Re-implement how the stats task works. Using Gulp to get the files
    is really slow. Taking upwards of ~20 seconds. The new method takes
    less than 2 seconds to search the same project.

--- gulpfile-main.js
+++ gulpfile-main.js
@@ -2459,94 +2459,141 @@ gulp.task("eol", function(done) {
  *
  * • Depending on project size, task might take time to run.
  *
+ * -c, --comprehensive [boolean]
+ *     By default all files are checked for. Providing this flag indicates
+ *     to use a more comprehensive code file extensions list.
+ *
+ * -w, --web [boolean]
+ *     By default all files are checked for. Providing this flag indicates
+ *     to use a exclusive web file extensions list.
+ *
+ * -l, --list [boolean]
+ *     Providing this flag indicates to print the list of file paths.
+ *
  * $ gulp stats
  *     Print file type breakdown.
+ *
+ * $ gulp stats --comprehensive
+ *     Print file type breakdown of code files.
+ *
+ * $ gulp stats --web
+ *     Print file type breakdown of common web file types.
+ *
+ * $ gulp stats --web --list
+ *     Print breakdown of common web file types and the file paths.
  */
 gulp.task("stats", function(done) {
 	var Table = require("cli-table2");
 
 	// Run yargs.
-	var __flags = yargs.option("all", {
-		type: "boolean"
-	}).argv;
+	var __flags = yargs
+		.option("comprehensive", {
+			alias: "c",
+			type: "boolean"
+		})
+		.option("web", {
+			alias: "w",
+			type: "boolean"
+		})
+		.option("list", {
+			alias: "l",
+			type: "boolean"
+		}).argv;
 
 	// Get flag value.
-	var all = __flags.all;
+	var comprehensive = __flags.c || __flags.comprehensive;
+	var web = __flags.w || __flags.web;
+	var list = __flags.l || __flags.list;
 
-	// Get all files excluding: node_modules/, .git/, and img/.
-	var files = [
-		// If the --all flag is provided use all the file types else
-		// only use the common web file types.
-		$paths["files_" + (all ? "code" : "common")],
-		bangify($paths.img_source),
-		bangify(globall($paths.node_modules_name)),
-		bangify(globall($paths.git))
-	];
-
-	var file_count = 0;
+	// This is where the used extensions will be stored.
 	var extensions = {};
+	var filter = "";
 
-	// Get needed files.
-	pump(
-		[
-			gulp.src(files, {
-				dot: true,
-				read: false
-			}),
-			$.fn(function(file) {
-				// Get the extension type.
-				var ext = path
-					.extname(file.path)
-					.toLowerCase()
-					.slice(1);
+	// If flags are provided use them as a filter. Else if flags are not
+	// provided include all project files in search.
+	if (web || comprehensive) {
+		// Prepare the extension types.
+		filter =
+			'--type="' +
+			$paths["files_" + (comprehensive ? "code" : web ? "common" : "")]
+				.match(/\{.*\}/)[0]
+				.replace(/\{|\}/g, "")
+				.replace(/,/g, " ") +
+			'"';
+	}
 
-				// Exclude any extension-less files.
-				if (!ext) {
-					return;
-				}
+	// Run the files task with the type flag from via cmd-node to not
+	// repeat the logic again.
+	cmd.get(`${GULPCLI} files ${filter}`, function(err, data) {
+		if (err) {
+			throw err;
+		}
 
-				var ext_count = extensions[ext];
+		// Get the lines with path files only.
+		var files = data.match(/(=>\s+)([^\s]+)(\s)(\d+(.\d+)? \w+)/g);
 
-				file_count++;
+		// Further clean the output to only get the file paths.
+		files = files.filter(function(line) {
+			var filepath = line.match(/(=>\s+)([^\s]+)(\s)(\d+(.\d+)? \w+)/)[2];
 
-				if (ext_count === undefined) {
-					// Does not exist, so start extension count.
-					extensions[ext] = 1;
-				} else {
-					// Already exists just increment the value.
-					extensions[ext] = ++ext_count;
-				}
-			})
-		],
-		function() {
-			// Instantiate.
-			var table = new Table({
-				head: ["Extensions", `Count (${file_count})`, "% Of Project"],
-				style: { head: ["green"] }
-			});
+			// Get the extension type.
+			var ext = extension({ path: filepath });
 
-			// Add data to table.
-			for (var ext in extensions) {
-				if (extensions.hasOwnProperty(ext)) {
-					var count = +extensions[ext];
-					table.push([
-						ext.toUpperCase(),
-						count,
-						Math.round(count / file_count * 100)
-					]);
-				}
+			// Exclude any extension-less files.
+			if (!ext) {
+				return false;
 			}
 
-			// Sort table descendingly.
-			table.sort(function(a, b) {
-				return b[2] - a[2];
-			});
+			// Get the extensions count for the current extension from the
+			// extensions object.
+			var ext_count = extensions[ext];
 
-			print(table.toString());
+			// If a counter does not exist for this extension, start one.
+			if (!ext_count) {
+				// Does not exist, so start extension count.
+				extensions[ext] = 1;
+			} else {
+				// Already exists just increment the value.
+				extensions[ext] = ++ext_count;
+			}
 
-			done();
+			return filepath;
+		});
+
+		// Get the files array length.
+		var file_count = files.length;
+
+		// Instantiate.
+		var table = new Table({
+			head: ["Extensions", `Count (${file_count})`, "% Of Project"],
+			style: { head: ["green"] }
+		});
+
+		// Add the data to the table.
+		for (var ext in extensions) {
+			if (extensions.hasOwnProperty(ext)) {
+				var count = +extensions[ext];
+				table.push([
+					"." + ext.toLowerCase(), // The extension name.
+					count, // The times the extension is used.
+					// The % relative to the project the extension is used.
+					Math.round(count / file_count * 100)
+				]);
+			}
 		}
-	);
+
+		// Sort table descendingly.
+		table.sort(function(a, b) {
+			return b[2] - a[2];
+		});
+
+		// Highlight data string.
+		if (list) print(cli_highlight(data));
+
+		print(table.toString());
+
+		done();
+	});
 });
 
 // -----------------------------------------------------------------------------
@@ -2693,7 +2740,7 @@ gulp.task("files", function(done) {
 		// argument is provided.
 		if (types) {
 			files = files.filter(function(filepath) {
-				return ext({ path: filepath }, types);
+				return extension({ path: filepath }, types);
 			});
 		}
 
