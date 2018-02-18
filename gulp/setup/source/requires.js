a519874c6057d50f0ae4eb69b8c2598016e8994b
cgabriel5 <carlosgabriel71@yahoo.com>
2018-02-17 17:23:30 -0800

    refactor: Update requires.js, gulpfile-setup.js
    Remove unneeded modules from the setup.

--- gulp/setup/source/requires.js
+++ gulp/setup/source/requires.js
@@ -14,29 +14,8 @@ var path = require("path");
 // Lazy load gulp plugins.
 var $ = require("gulp-load-plugins")({
 	rename: {
-		"gulp-if": "gulpif",
-		"gulp-markdown": "marked",
-		"gulp-purifycss": "purify",
-		"gulp-clean-css": "clean_css",
-		"gulp-json-sort": "json_sort",
-		"gulp-jsbeautifier": "beautify",
-		"gulp-minify-html": "minify_html",
 		"gulp-prettier-plugin": "prettier",
-		"gulp-inject-content": "injection",
-		"gulp-real-favicon": "real_favicon",
-		"gulp-strip-json-comments": "strip_jsonc"
-	},
-	postRequireTransforms: {
-		json_sort: function(plugin) {
-			return plugin.default;
-		},
-		uglify: function() {
-			// By default es-uglify is used to uglify JS.
-			// [https://stackoverflow.com/a/45554108]
-			var uglifyjs = require("uglify-es");
-			var composer = require("gulp-uglify/composer");
-			return composer(uglifyjs, console);
-		}
+		"gulp-inject-content": "injection"
 	}
 });
 
@@ -45,11 +24,11 @@ var pump = require("pump");
 var chalk = require("chalk");
 var cmd = require("node-cmd");
 var json = require("json-file");
-var sgit = require("simple-git/promise");
 var inquirer = require("inquirer");
 var jsonc = require("comment-json");
 var sequence = require("run-sequence");
 var license = require("create-license");
+var sgit = require("simple-git/promise");
 var alphabetize = require("alphabetize-object-keys");
 
 // Project utils.
