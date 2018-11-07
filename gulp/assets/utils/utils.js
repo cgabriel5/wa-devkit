c914296cdfce7514a28368b6981ebf016ae40916
cgabriel5 <carlosgabriel71@yahoo.com>
2018-11-07 14:41:59 -0800

    refactor: Modify the behavior of the format utility function
    - Function can now take n arguments. Arguments will be turned into
    a replacement map.

--- gulp/assets/utils/utils.js
+++ gulp/assets/utils/utils.js
@@ -213,13 +213,39 @@ var uri = function(params) {
  * Formats template with provided data object.
  *
  * @param {string} template - The template to use.
- * @param {object} data - The object containing the data to replace
- *     placeholders with.
- * @return {undefined} - Nothing.
+ * @param {object|*n} data - The object containing the data to replace
+ *     placeholders with. Or n amount of arguments.
+ * @return {string} - The formatted template.
  */
 var format = function(template, data) {
+	// If an object containing the replacement map is not provided as the
+	// second argument, then get all arguments from 1 to N and create the
+	// replacement map.
+	var rmap = {};
+	var normalized;
+	if (typeof arguments[1] !== "object") {
+		// Set flag.
+		normalized = true;
+
+		// Create the replacement map.
+		for (let i = 1, l = arguments.length; i < l; i++) {
+			// Store the arguments.
+			rmap[i] = arguments[i];
+		}
+		// Finally, reset variable.
+		data = rmap;
+	}
+
 	return template.replace(/\{\{\#(.*?)\}\}/g, function(match) {
+		// Remove formating decorations.
 		match = match.replace(/^\{\{\#|\}\}$/g, "");
+
+		// If using an index based replacement map, remove everything
+		// but numbers.
+		if (normalized) {
+			match = match.replace(/[^0-9]/g, "");
+		}
+
 		// Lookup replacement value.
 		let lookup = data[match];
 		// If the value is anything but undefined or null then use it as a
@@ -227,9 +253,9 @@ var format = function(template, data) {
 		// to strings before replacement. This allows for falsy values
 		// like "0" (zero) and "" (an empty string) to be used as substitute
 		// values.
-		return lookup !== undefined || lookup !== null
-			? String(lookup)
-			: `{{#${match}}}`;
+		return lookup === undefined || lookup === null
+			? `{{#${match}}}`
+			: String(lookup);
 	});
 };
 
